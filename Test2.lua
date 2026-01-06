edb341c1efd4635adeac8d7201318a722ef72f61
tru897tr <truongthikimthanh.cx261984@gmail.com>
2026-01-06 23:31:01 +0700

    Update Test2.lua

--- Test2.lua
+++ Test2.lua
@@ -1,4 +1,4 @@
-local TweenService = game:GetService("TweenService")
+
 local UserInputService = game:GetService("UserInputService")
 local RunService = game:GetService("RunService")
 local Players = game:GetService("Players")
